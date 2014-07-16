dd5d7061cef4dab5cd897d99fb81698e3174edce
Rob Misiorowski <rmisio@yahoo.com>
2014-07-16 11:42:29 -0500

    starting about me quiz form validation

--- app/scripts/views/about-page.js
+++ app/scripts/views/about-page.js
@@ -14,6 +14,9 @@ define([
         template: JST['app/scripts/templates/about-page.ejs'],
 
         events: {
+            'submit form': 'submitQuiz',
+            'reset form': 'resetQuiz',
+            'change form input': 'inputChange'
         },
 
         initialize: function (options) {
@@ -22,8 +25,43 @@ define([
             app.appView.renderPage(this);            
         },
 
+        resetQuiz: function (e) {
+            this.$('form .has-error-required')
+                .removeClass('has-error-required');
+            this.$submitButton.removeAttr('disabled');
+        },
+
+        inputChange: function (e) {
+            $(e.target).parents('.form-group')
+                .removeClass('has-error-required');
+
+            if (!this.$('form .has-error-required').length) {
+                this.$submitButton.removeAttr('disabled');
+            }
+        },
+
+        submitQuiz: function (e) {
+            var self = this,
+                disabledSubmit = false;
+
+            e.preventDefault();
+
+            this.$inputs = this.$inputs ||
+                this.$('input[type=radio]');
+
+            // makes sure all fields have a selection
+            this.$('input[type=radio]:not(:checked)').each(function (i, input) {
+                !disabledSubmit && self.$submitButton.attr('disabled', '');
+                $(input).parents('.form-group')
+                    .addClass('has-error-required');
+            });
+        },
+
         render: function (context) {
             this.$el.html(util.template(this.template, context));
+
+            // cache selectors
+            this.$submitButton = this.$('button[type=submit]');
 
             return this;
         }
