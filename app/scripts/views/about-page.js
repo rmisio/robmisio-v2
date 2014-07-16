aca578f148212cefdda26b5ae0176d0d6907cdeb
Rob Misiorowski <rmisio@yahoo.com>
2014-07-16 15:45:52 -0500

    quiz grading functionality

--- app/scripts/views/about-page.js
+++ app/scripts/views/about-page.js
@@ -25,10 +25,24 @@ define([
             app.appView.renderPage(this);            
         },
 
+        quizScrollTop: function () {
+            $('html, body').animate({
+                scrollTop: this.$('#quiz-results-container').offset().top
+            }, 500);
+        },
+
         resetQuiz: function (e) {
-            this.$('form .has-error-required')
-                .removeClass('has-error-required');
-            this.$submitButton.removeAttr('disabled');
+            this.$('form')
+                .find('.correct-answer, .incorrect-answer')
+                .removeClass('correct-answer incorrect-answer')
+                .end()
+                .find('.has-error-required')
+                .removeClass('has-error-required')
+                .end()
+                .find('input[type=radio]')
+                .prop('disabled', false);
+            this.$submitButton.prop('disabled', false);
+            this.$('.quiz-results').hide();
         },
 
         inputChange: function (e) {
@@ -36,25 +50,55 @@ define([
                 .removeClass('has-error-required');
 
             if (!this.$('form .has-error-required').length) {
-                this.$submitButton.removeAttr('disabled');
+                this.$submitButton.prop('disabled', false);
             }
         },
 
         submitQuiz: function (e) {
             var self = this,
-                disabledSubmit = false;
+                requiredError = false,
+                score;
 
             e.preventDefault();
-
-            this.$inputs = this.$inputs ||
-                this.$('input[type=radio]');
+            this.$submitButton.prop('disabled', true);
 
             // makes sure all fields have a selection
-            this.$('input[type=radio]:not(:checked)').each(function (i, input) {
-                !disabledSubmit && self.$submitButton.attr('disabled', '');
-                $(input).parents('.form-group')
-                    .addClass('has-error-required');
+            this.$('.form-group').each(function (i, val) {
+                var $inputs = $('input[type=radio]', this);
+
+                if ($inputs.length === $('input[type=radio]:not(:checked)', this).length) {
+                    if (!requiredError) {
+                        requiredError = true;
+                        self.$submitButton.prop('disabled', true);
+                    }
+                    $(this).addClass('has-error-required');                    
+                }
             });
+
+            if (!requiredError) {
+                // check if answers are correct
+                this.$('input[type=radio]')
+                    .prop('disabled', true)
+                    .filter(':checked').each(function (i, val) {
+                        var $input = $(this);
+                        
+                        if ($input.attr('data-answer')) {
+                            $input.parents('.radio')
+                                .addClass('correct-answer');
+                        } else {
+                            $input.parents('.radio')
+                                .addClass('incorrect-answer');
+                        }
+                    });
+
+                score = Math.round(this.$('.correct-answer').length /
+                    this.$('.form-group').length * 100);
+                this.$('.quiz-results')
+                    .text(score + '%')
+                    .show();
+            }
+
+            this.quizScrollTop();
         },
 
         render: function (context) {
