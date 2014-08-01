e7c4c09354832ecf9ae0ac0ac3785eae8a4ad3af
Rob Misiorowski <rmisio@yahoo.com>
2014-08-01 14:13:52 -0500

    removing JPG's from rev task

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,7 +285,7 @@ module.exports = function (grunt) {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+                        '<%= yeoman.dist %>/images/{,*/}*.{png,gif,webp}',
                         '/styles/fonts/{,*/}*.*',
                         'bower_components/sass-bootstrap/fonts/*.*'
                     ]
