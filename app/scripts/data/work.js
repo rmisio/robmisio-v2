a4885953c30dc1c17bff93ea4c137c6a083cfc11
Rob Misiorowski <rmisio@yahoo.com>
2016-01-04 10:46:41 -0800

    various minor tweaks

--- app/scripts/data/work.js
+++ app/scripts/data/work.js
@@ -24,7 +24,7 @@ define([
           'lifeforecast/flow.jpg'
         ],
         description: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel velit velit. Suspendisse ac porttitor diam. Suspendisse sit amet sagittis velit. Integer nisi urna, mattis vitae risus eget, scelerisque pharetra nunc. Sed egestas ut sem eu ullamcorper. Nulla nulla orci, consectetur et sem sit amet, ornare posuere ante. Nullam eget ipsum vulputate arcu faucibus rutrum sit amet at ex. Etiam a est risus. Vestibulum porta viverra dolor at malesuada.</p><p>Nunc finibus scelerisque erat, at lobortis libero lacinia sed. Cras interdum libero nec arcu lobortis, sit amet accumsan tellus blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in eleifend arcu. Nam feugiat maximus urna, sed feugiat lacus tempus quis. Vivamus lectus ipsum, gravida in commodo pellentesque, accumsan eu ante. Nulla sodales mollis mi sed posuere. Proin molestie enim quam, a congue eros sodales et. Praesent eleifend nunc vitae mauris consequat fringilla. Sed tincidunt nisl vel neque iaculis, sed dapibus risus feugiat. Nam convallis purus diam.</p>'
-      }      
+      }
     ];
 
     return work;
